335316d6c722d216fe04386f06aef8ad0c3e1911
joy <joy@joys-MacBook-Pro.local>
2018-07-24 18:20:44 +0900

    add function to wellcome message removing when welcomeMsgQ is over. add function to remove message which contains url and photos.

---
+++ app/queue.js
@@ -0,0 +1,30 @@
+
+function Queue() {
+    this.data = [];
+}
+
+Queue.prototype.add = function (record) {
+    this.data.unshift(record);
+};
+
+Queue.prototype.remove = function () {
+    if(this.data.length >0) {
+        var last = this.last();
+        this.data.pop();
+        return last;
+    }
+};
+
+Queue.prototype.first = function () {
+    return this.data[0];
+};
+
+Queue.prototype.last = function () {
+    return this.data[this.data.length - 1];
+};
+
+Queue.prototype.size = function () {
+    return this.data.length;
+};
+
+module.exports = Queue;
