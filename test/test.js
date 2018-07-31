f0558f7aa93eaa1390b2e5cd7b52a9606ebdfe46
sw-yoonsung <sw.yoonsung@gmail.com>
2018-07-31 10:21:46 +0900

    remove start sample keyboard.

--- test/test.js
+++ test/test.js
@@ -1,33 +1,61 @@
 const fs = require('fs');
-const configJson                    = JSON.parse(fs.readFileSync('../config/config.json', 'utf8'));
+const configJson = JSON.parse(fs.readFileSync('../config/config.json', 'utf8'));
 
-const keyboard                      = configJson['keyboard'];
-let keyboardMap                     = new Map();
+const keyboard = configJson['keyboard'];
+let keyboardMap = new Map();
 
 const objectTypes = require('object-types');
 
-arrayTest();
+var result = arrayTest();
 
-function logJson(arr, tap) {
-    var type= objectTypes(arr);
+console.log(result);
+console.log(keyboardMap);
+console.log("------------append map--------------");
+console.log(appendMap());
 
-    for(var i in arr){
-        var key =i;
-        // type = objectTypes(key);
-        var val = arr[key];
-        type = objectTypes(val);
-        console.log(tap + key + " : " + val);
-        if(val.length > 0) {
-            var nextTap = tap + "  ";
-            logJson(val, nextTap);
+function appendMap(){
+    var map = {"1" : 1, "2" : 2};
+    var oldMap = {"3":3, "4":4};
+    var newMap = Object.assign(map, oldMap);
+    console.log(map);//same with newMap
+    return newMap;
+}
+
+function logJson(arr) {
+    var type = objectTypes(arr);
+    var keyboardResult = [];
+    arr.forEach(obj => {
+        type = objectTypes(obj);
+        if (type === 'object') {
+            let keys = Object.keys(obj);
+            keyboardResult.push(keys);
+            keys.forEach(key => {
+                keyboardMap.set(key, obj[key]);
+                console.log(key + " : " + obj[key]);
+            });
+        } else if (type === 'array') {
+            // second depth for one line multi button.
+            var subKeyboard = [];
+            obj.forEach(subObj => {
+                let subType = objectTypes(subObj);
+                if (subType === 'object') {
+                    let keys = Object.keys(subObj);
+                    keys.forEach(key => {
+                        subKeyboard.push(key);
+                        keyboardMap.set(key, subObj[key]);
+                        console.log(key + " : " + subObj[key]);
+                    });
+                }
+            });
+            keyboardResult.push(subKeyboard);
         }
-    }
-
+    });
+    return keyboardResult;
 }
 
 function arrayTest() {
-    const arr = [{"key1" : "val1"}, [{"key2":"val2", "key2_2": "val2_2"},{"key3":"val3"}]];
-    logJson(arr, "");
+    const arr = [{"key1": "val1"}, [{"key2": "val2", "key2_2": "val2_2"}, {"key3": "val3"}]];
+    return logJson(arr, "");
     // arr.forEach((cur) => {
     //     let obj = cur;
     //     console.log(obj.key + " : " + obj.value);
